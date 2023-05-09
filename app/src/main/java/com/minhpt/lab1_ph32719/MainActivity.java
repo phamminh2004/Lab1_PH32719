f9d2a27fbf3259de081966acc268b7ce4b3f4e87
Minh <phamminh20804@gmail.com>
2023-05-09 21:12:10 +0700

    Initial commit

---
+++ app/src/main/java/com/minhpt/lab1_ph32719/MainActivity.java
@@ -0,0 +1,14 @@
+package com.minhpt.lab1_ph32719;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
